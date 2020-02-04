badaadd367c03c40b4436ad4a5ef2f1c8dae480e
Your Name Comes Here <archie808@hotmail.com>
2020-02-04 14:47:57 -0700

    Implemented storage and retrieval of Apod database.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/model/dao/ApodDao.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/model/dao/ApodDao.java
@@ -1,39 +1,42 @@
 package edu.cnm.deepdive.nasaapod.model.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 @Dao
-public interface ApodDao {  // interface methods are always abstract (public).
+public interface ApodDao {  // interface methods are always abstract.
   // C.R.U.D = create, read, update, delete.
   @Insert
-  long insert(Apod apod);
+  Single<Long> insert(Apod apod);
 
   @Insert
-  List<Long> insert(Apod... apods);
+  Single<List<Long>> insert(Apod... apods);
 
   @Delete
-  int delete(Apod apod);  // int is the number of objects being deleted.
+  Single<Integer> delete(Apod apod);  // int is the number of objects being deleted.
 
   @Delete
-  int delete(Collection<Apod> apods);
+  Single<Integer> delete(Collection<Apod> apods);
 
   @Delete
-  int delete(Apod... apods);
+  Single<Integer> delete(Apod... apods);
 
   @Query("SELECT * FROM Apod ORDER BY date DESC")
-  List<Apod> select();
+  LiveData<List<Apod>> select();
 
   @Query("SELECT * FROM Apod WHERE date = :date")
-  Apod select(Date date);
+  Maybe<Apod> select(Date date);  // 'Maybe' is a generic object, whether or not there is data.
 
   @Query("SELECT * FROM Apod WHERE apod_id = :id")
-  Apod select(long id);
+  Single<Apod> select(long id);
 
 }
